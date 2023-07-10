805c08fbbde3a8f32a066a884953f56c4e327de8
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-07-09 23:58:28 -0500

    Redis Env Variables

--- auth-service/src/config/redis-pubsub.ts
+++ auth-service/src/config/redis-pubsub.ts
@@ -1,9 +1,10 @@
 import { RedisPubSub } from "graphql-redis-subscriptions";
 import Redis from "ioredis";
+import { ENV } from "../env";
 
 export const redisConnection = async () => {
   const options = {
-    password: "gql-subpub",
+    password: ENV.REDIS_PASS,
     port: 6379,
     host: "localhost",
     retryStrategy: (times: number) => {
@@ -11,7 +12,7 @@ export const redisConnection = async () => {
       return Math.min(times * 50, 2000);
     },
   };
-  console.log("INFO: connecting to redis ")
+  console.log("INFO: connecting to redis ");
   return new RedisPubSub({
     subscriber: new Redis(options),
     publisher: new Redis(options),
--- auth-service/src/env.ts
+++ auth-service/src/env.ts
@@ -7,4 +7,5 @@ export const ENV = {
   PORT: process.env.SERVER_PORT || 3000,
   MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017",
   JWT_SECRET: process.env.JWT_SECRET || "",
+  REDIS_PASS: process.env.REDIS_PASS || "",
 };
