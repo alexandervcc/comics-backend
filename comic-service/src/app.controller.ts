d8155a6e6f7c721263575c813c85c7a0cc61aad2
Alex Charco Cuenca <alex.charco.25@gmail.com>
2023-08-08 15:31:55 -0500

    Implement Guard

--- comic-service/src/app.controller.ts
+++ comic-service/src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  //@UseGuards(AuthGuard)
   getHello(): string {
     return this.appService.getHello();
   }
