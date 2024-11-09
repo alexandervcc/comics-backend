e9951b22e91b88d4d9bbba65d4d9533ecdf19c62
alexandervcc <alex.charco.25@gmail.com>
2024-11-09 17:13:24 -0500

    Add validation for comic creation dto

--- comic-service/src/modules/comics/dto/ComicData.ts
+++ comic-service/src/modules/comics/dto/ComicData.ts
@@ -1,7 +1,18 @@
+import { ObjectId } from 'mongoose';
 import { ComicData } from '../model/comic';
+import { ArrayNotEmpty, IsEmpty, IsIn, IsNotEmpty } from 'class-validator';
 
 export class ComicDataDto implements ComicData {
+  @IsEmpty()
+  _id: ObjectId;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsIn(['shonen'])
+  @ArrayNotEmpty()
   genre: string[];
+
+  @ArrayNotEmpty()
   author: string[];
 }
